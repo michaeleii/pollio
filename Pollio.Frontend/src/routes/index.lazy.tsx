4db12055ab02a5cfb26af967f28293c0be399bd1
Michael Lei <michaelleie0ca@gmail.com>
2024-02-18 19:01:38 -0800

    Add Poll components

--- Pollio.Frontend/src/routes/index.lazy.tsx
+++ Pollio.Frontend/src/routes/index.lazy.tsx
@@ -1,6 +1,7 @@
 import MainWrapper from "@/components/main-wrapper";
 import useFetchPolls from "@/hooks/useFetchPolls";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { Poll } from "@/types/types";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
@@ -10,18 +11,27 @@ function Index() {
   const { polls, isPending, error } = useFetchPolls();
   return (
     <MainWrapper>
-      <h1 className="text-4xl font-bold">All Polls</h1>
       {isPending ? (
         <p>Loading...</p>
       ) : error ? (
         <p>Error: {error.message}</p>
       ) : polls ? (
-        <ul>
-          {polls.map((poll) => (
-            <li key={poll.id}>{poll.question}</li>
-          ))}
-        </ul>
+        <PollList polls={polls} />
       ) : null}
     </MainWrapper>
   );
+}
+
+function PollList({ polls }: { polls: Poll[] }) {
+  return (
+    <div>
+      {polls.map((poll) => (
+        <PollItem poll={poll} />
+      ))}
+    </div>
+  );
+}
+
+function PollItem({ poll }: { poll: Poll }) {
+  return <div>{poll.question}</div>;
 }
