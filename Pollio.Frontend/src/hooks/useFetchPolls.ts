ddfdf4752d13a51cbf2872448c26e49b3ea02f43
Michael Lei <michaelleie0ca@gmail.com>
2024-02-18 19:01:08 -0800

    Add hook to fetch polls

--- Pollio.Frontend/src/hooks/useFetchPolls.ts
+++ Pollio.Frontend/src/hooks/useFetchPolls.ts
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import { Poll } from "../types/types";
 
 export default function useFetchPolls() {
   const { data, error, isPending } = useQuery({
     queryKey: ["polls"],
     queryFn: async () => {
       const response = await fetch("/api/poll");
-      return response.json();
+      return response.json() as Promise<Poll[]>;
     },
   });
   return { polls: data, error, isPending };
