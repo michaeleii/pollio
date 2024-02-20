ada26dd80174909588a8dd309e00b849d3e6af96
Michael Lei <michaelleie0ca@gmail.com>
2024-02-19 21:39:27 -0800

    Refactor: Add primary constructor

--- Pollio.Web/Controllers/PollController.cs
+++ Pollio.Web/Controllers/PollController.cs
@@ -1,127 +1,117 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pollio.Web.DTO;
 using Pollio.Web.Models;
 
-namespace Pollio.Web.Controllers
+namespace Pollio.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PollController(PollContext context) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PollController : ControllerBase
+    private readonly PollContext _context = context;
+
+    // GET: api/Poll
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PollDTO>>> GetPolls()
     {
-        private readonly PollContext _context;
-
-        public PollController(PollContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Poll
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<PollDTO>>> GetPolls()
-        {
-            return await _context.Polls
-                .Include(p => p.User)
-                .Select(p => new PollDTO
+        return await _context.Polls
+            .Include(p => p.User)
+            .Select(p => new PollDTO
+            {
+                Id = p.Id,
+                Question = p.Question,
+                CreatedAt = p.CreatedAt,
+                User = new PollDTO.UserDTO
                 {
-                    Id = p.Id,
-                    Question = p.Question,
-                    CreatedAt = p.CreatedAt,
-                    User = new PollDTO.UserDTO
-                    {
-                        Id = p.User.Id,
-                        Username = p.User.Username,
-                    },
-                    Options = p.Options.Select(o => new PollDTO.OptionDTO
-                    {
-                        Id = o.Id,
-                        Text = o.Text,
-                        CreatedAt = o.CreatedAt,
-                    }).ToList(),
-                })
-                .ToListAsync();
+                    Id = p.User.Id,
+                    Username = p.User.Username,
+                },
+                Options = p.Options.Select(o => new PollDTO.OptionDTO
+                {
+                    Id = o.Id,
+                    Text = o.Text,
+                    CreatedAt = o.CreatedAt,
+                }).ToList(),
+            })
+            .ToListAsync();
+    }
+
+    // GET: api/Poll/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Poll>> GetPoll(int id)
+    {
+        var poll = await _context.Polls.FindAsync(id);
+
+        if (poll == null)
+        {
+            return NotFound();
         }
 
-        // GET: api/Poll/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Poll>> GetPoll(int id)
-        {
-            var poll = await _context.Polls.FindAsync(id);
+        return poll;
+    }
 
-            if (poll == null)
+    // PUT: api/Poll/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutPoll(int id, Poll poll)
+    {
+        if (id != poll.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(poll).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!PollExists(id))
             {
                 return NotFound();
             }
-
-            return poll;
-        }
-
-        // PUT: api/Poll/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPoll(int id, Poll poll)
-        {
-            if (id != poll.Id)
+            else
             {
-                return BadRequest();
+                throw;
             }
-
-            _context.Entry(poll).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PollExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
         }
 
-        // POST: api/Poll
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Poll>> PostPoll(Poll poll)
+        return NoContent();
+    }
+
+    // POST: api/Poll
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Poll>> PostPoll(Poll poll)
+    {
+        _context.Polls.Add(poll);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetPoll", new { id = poll.Id }, poll);
+    }
+
+    // DELETE: api/Poll/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePoll(int id)
+    {
+        var poll = await _context.Polls.FindAsync(id);
+        if (poll == null)
         {
-            _context.Polls.Add(poll);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetPoll", new { id = poll.Id }, poll);
+            return NotFound();
         }
 
-        // DELETE: api/Poll/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePoll(int id)
-        {
-            var poll = await _context.Polls.FindAsync(id);
-            if (poll == null)
-            {
-                return NotFound();
-            }
+        _context.Polls.Remove(poll);
+        await _context.SaveChangesAsync();
 
-            _context.Polls.Remove(poll);
-            await _context.SaveChangesAsync();
+        return NoContent();
+    }
 
-            return NoContent();
-        }
-
-        private bool PollExists(int id)
-        {
-            return _context.Polls.Any(e => e.Id == id);
-        }
+    private bool PollExists(int id)
+    {
+        return _context.Polls.Any(e => e.Id == id);
     }
 }
+
