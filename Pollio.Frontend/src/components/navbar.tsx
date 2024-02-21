a48a3d98795a8cf6628a715b0b5281193c0d64eb
Michael Lei <michaelleie0ca@gmail.com>
2024-02-20 19:38:41 -0800

    Add user display to navbar

--- Pollio.Frontend/src/components/navbar.tsx
+++ Pollio.Frontend/src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import { BarChartHorizontalBig } from "lucide-react";
+import { BarChartHorizontalBig, Circle } from "lucide-react";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
 
@@ -17,9 +17,59 @@ export default function Navbar() {
           <Link to="/create">
             <Button>Create Poll</Button>
           </Link>
+          <Profile />
           <ModeToggle />
         </nav>
       </div>
     </header>
+  );
+}
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import useSignalR from "@/hooks/useSignalR";
+function Profile() {
+  const { connection } = useSignalR("/r/pollhub");
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="flex items-center gap-2 relative">
+          <img
+            src="https://avatars.githubusercontent.com/u/57844588?v=4"
+            alt="michaellei"
+            className="w-10 h-10 rounded-full"
+          />
+          {connection ? (
+            <Circle
+              className="absolute -bottom-1 -right-1.5 fill-green-600 stroke-background stroke-[5px]"
+              size={18}
+            />
+          ) : (
+            <Circle
+              className="absolute -bottom-1 -right-1.5 fill-yellow-600 stroke-background stroke-[5px]"
+              size={18}
+            />
+          )}
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel className="flex flex-col">
+          <span>michaellei</span>
+          <span className="font-medium">
+            {connection ? (
+              <span className="text-green-600">Online</span>
+            ) : (
+              <span className="text-red-600">Connecting...</span>
+            )}
+          </span>
+        </DropdownMenuLabel>
+        {/* <DropdownMenuSeparator /> */}
+        {/* <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Logout</DropdownMenuItem> */}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
