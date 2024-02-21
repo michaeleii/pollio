da020adce4ae4246b06adf2ec08dcd623b831503
Michael Lei <michaelleie0ca@gmail.com>
2024-02-21 11:23:43 -0800

    Add static files for .net backend from vite build

---
+++ Pollio.Web/wwwroot/assets/trash-2-B3l4bbop.js
@@ -0,0 +1,6 @@
+import{f as c,g as r,r as o,c as n}from"./index-CTLQgVu5.js";function i(u){const{navigate:e}=c(),s=r({strict:!1,select:t=>t.pathname});return o.useCallback(({from:t,...a})=>e({from:a!=null&&a.to?s:void 0,...a}),[])}/**
+ * @license lucide-react v0.331.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=n("Trash2",[["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6",key:"4alrt4"}],["path",{d:"M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2",key:"v07s0e"}],["line",{x1:"10",x2:"10",y1:"11",y2:"17",key:"1uufr5"}],["line",{x1:"14",x2:"14",y1:"11",y2:"17",key:"xtxkd"}]]);export{l as T,i as u};
