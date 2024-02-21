377c0a120b08f4654b23e3a1fff4a8478ea37429
Michael Lei <michaelleie0ca@gmail.com>
2024-02-20 21:06:58 -0800

    Add kinde provider

--- Pollio.Frontend/src/components/providers.tsx
+++ Pollio.Frontend/src/components/providers.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "./providers/theme-provider";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import React, { Suspense } from "react";
 
 // Create a client
@@ -20,15 +21,22 @@ const TanStackRouterDevtools =
 
 export default function Providers({ children }: ProvidersProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        {children}
-      </ThemeProvider>
-      <Suspense>
-        <TanStackRouterDevtools />
-      </Suspense>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <KindeProvider
+      clientId="5b88e740d3ec47ccaf2bd331d5923f3f"
+      domain="https://pollio.kinde.com"
+      redirectUri="http://localhost:5173"
+      logoutUri="http://localhost:5173"
+    >
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          {children}
+        </ThemeProvider>
+        <Suspense>
+          <TanStackRouterDevtools />
+        </Suspense>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </KindeProvider>
   );
 }
 
